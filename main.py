d30f784fe57a2e68a30303710defb1c9337a0ff5
lolguy66 <alexanderflotti@gmail.com>
2020-01-30 16:26:01 +0000

    completed

--- main.py
+++ main.py
@@ -14,7 +14,56 @@ for markdown_post in os.listdir('content'):
 
     
 POSTS = {
-    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%d-%m-%Y'), reverse=True)
+    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
+}
+
+env = Environment(loader=PackageLoader('main', 'templates'))
+home_template = env.get_template('home.html')
+post_template = env.get_template('post.html')
+
+posts_metadata = [POSTS[post].metadata for post in POSTS]
+tags = [post['tags'] for post in posts_metadata]
+home_html = home_template.render(posts=posts_metadata, tags=tags)
+
+with open('../recipes/index.html', 'w') as file:
+    file.write(home_html)
+
+for post in POSTS:
+    post_metadata = POSTS[post].metadata
+
+    post_data = {
+        'content': POSTS[post],
+        'title': post_metadata['title'],
+        'date': post_metadata['date'],
+        'thumbnail': post_metadata['thumbnail']
+    }
+
+    post_html = post_template.render(post=post_data)
+    post_file_path = '../recipes/posts/{slug}.html'.format(slug=post_metadata['slug'])
+
+    os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
+    with open(post_file_path, 'w') as file:
+        file.write(post_html)
+
+
+"""
+import os
+from datetime import datetime
+from jinja2 import Environment, PackageLoader
+from markdown2 import markdown
+
+
+POSTS = {}
+
+for markdown_post in os.listdir('content'):
+    file_path = os.path.join('content', markdown_post)
+
+    with open(file_path, 'r') as file:
+        POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
+
+    
+POSTS = {
+    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
 }
 # skilgreina template sem á að nota
 env = Environment(loader=PackageLoader('main', 'templates'))
@@ -25,7 +74,7 @@ posts_metadata = [POSTS[post].metadata for post in POSTS]
 tags = [post['tags'] for post in posts_metadata]
 home_html = home_template.render(posts=posts_metadata, tags=tags)
 # ekki output heldur upp úr vinnumöppuni og á nýja
-with open('../recipes2/posts/index.html', 'w') as file:
+with open('../recipes/posts/index.html', 'w') as file:
     file.write(home_html)
 
 for post in POSTS:
@@ -38,8 +87,9 @@ for post in POSTS:
     }
 
     post_html = post_template.render(post=post_data)
-    post_file_path = 'output/posts/{slug}.html'.format(slug=post_metadata['slug'])
+    post_file_path = '../recipes2/posts/{slug}.html'.format(slug=post_metadata['slug'])
 
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
         file.write(post_html)
+"""
